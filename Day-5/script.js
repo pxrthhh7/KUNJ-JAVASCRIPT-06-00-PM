9e5c5b816b192481c62b5a2482ce2754ecb6ae74
pxrthhh7 <parthhadiyal4242@gmail.com>
2025-03-31 11:58:40 +0530

    Day-5 Added

---
+++ Day-5/script.js
@@ -0,0 +1,26 @@
+function on()
+{
+    let box = document.getElementById("hp")
+
+    box.style.backgroundColor = "yellow"
+}
+
+function off()
+{
+    let box = document.getElementById("hp")
+
+    box.style.backgroundColor = "black"
+}
+
+function round()
+{
+    let box = document.getElementById("hp")
+
+    box.style.borderRadius = "50%"
+}
+function squre()
+{
+    let box = document.getElementById("hp")
+
+    box.style.borderRadius = "0%"
+}
