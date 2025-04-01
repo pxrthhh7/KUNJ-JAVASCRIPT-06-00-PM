183eb7124572c04602229df96b04bd9d013f3c41
pxrthhh7 <parthhadiyal4242@gmail.com>
2025-04-01 15:09:00 +0530

    Day-12 Clock Updated

--- Day-12/Clock/script.js
+++ Day-12/Clock/script.js
@@ -8,40 +8,86 @@ let hours = now.getHours()
 let minutes = now.getMinutes()
 let seconds = now.getSeconds()
 
-console.log(hours,minutes,seconds)
-
 setInterval(() => {
-    if (seconds < 60) 
-        {
-            seconds++   
-            console.log(hours,minutes,seconds) 
+    if (seconds < 60) {
+        seconds++
+        if (hours <= 9) {
+            print1.innerText = "0" + hours
+        }
+        else {
             print1.innerText = hours
+        }
+
+        if (minutes <= 9) {
+            print2.innerText = "0" + minutes;
+        }
+        else {
             print2.innerText = minutes;
+        }
+
+        if (seconds <= 9) {
+            print3.innerText = "0" + seconds;
+        }
+        else {
             print3.innerText = seconds;
         }
-        else if (minutes < 60) // 59 < 60
-        {
-            minutes ++
-            seconds = 1;    
-            console.log(hours,minutes,seconds) 
+    }
+    else if (minutes < 60) // 59 < 60
+    {
+        minutes++
+        seconds = 1;
+        if (hours <= 9) {
+            print1.innerText = "0" + hours
+        }
+        else {
             print1.innerText = hours
+        }
+
+        if (minutes <= 9) {
+            print2.innerText = "0" + minutes;
+        }
+        else {
             print2.innerText = minutes;
+        }
+
+        if (seconds <= 9) {
+            print3.innerText = "0" + seconds;
+        }
+        else {
             print3.innerText = seconds;
         }
-        else if (hours < 24) // 23
-        {
-            hours++
-            minutes = 0    
-            console.log(hours,minutes,seconds) 
+    }
+    else if (hours < 24) // 23
+    {
+        hours++
+        minutes = 0
+
+        if (hours <= 9) {
+            print1.innerText = "0" + hours
+        }
+        else {
             print1.innerText = hours
+        }
+
+        if (minutes <= 9) {
+            print2.innerText = "0" + minutes;
+        }
+        else {
             print2.innerText = minutes;
+        }
+
+        if (seconds <= 9) {
+            print3.innerText = "0" + seconds;
+        }
+        else {
             print3.innerText = seconds;
         }
-        else
-        {
-            hours = 0
-            minutes = 0
-            seconds = 0
-        } 
-            
+
+    }
+    else {
+        hours = 0
+        minutes = 0
+        seconds = 0
+    }
+
 }, 1000);
