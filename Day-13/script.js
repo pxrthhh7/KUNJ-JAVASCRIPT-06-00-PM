c95a809f664305c6c847518b1640f547614e1799
pxrthhh7 <parthhadiyal4242@gmail.com>
2025-04-04 11:17:25 +0530

    Day-13 Added

---
+++ Day-13/script.js
@@ -0,0 +1,79 @@
+let box = [
+    {
+        name: "parth",
+        mail: "parth987@gmail.com",
+        password: "parth@456"
+    },
+    {
+        name: "rahul",
+        mail: "rahul567@gmail.com",
+        password: "rahul@789"
+    },
+    {
+        name: "sneha",
+        mail: "sneha432@gmail.com",
+        password: "sneha@321"
+    },
+    {
+        name: "arjun",
+        mail: "arjun654@gmail.com",
+        password: "arjun@852"
+    },
+    {
+        name: "meera",
+        mail: "meera789@gmail.com",
+        password: "meera@963"
+    },
+    {
+        name: "rohit",
+        mail: "rohit321@gmail.com",
+        password: "rohit@147"
+    },
+    {
+        name: "priya",
+        mail: "priya258@gmail.com",
+        password: "priya@369"
+    },
+    {
+        name: "akash",
+        mail: "akash741@gmail.com",
+        password: "akash@852"
+    },
+    {
+        name: "neha",
+        mail: "neha852@gmail.com",
+        password: "neha@753"
+    },
+    {
+        name: "vivek",
+        mail: "vivek963@gmail.com",
+        password: "vivek@159"
+    },
+    {
+        name: "deepa",
+        mail: "deepa147@gmail.com",
+        password: "deepa@258"
+    },
+    {
+        name: "sumit",
+        mail: "sumit369@gmail.com",
+        password: "sumit@951"
+    },
+    {
+        name: "ananya",
+        mail: "ananya753@gmail.com",
+        password: "ananya@852"
+    },
+    {
+        name: "gautam",
+        mail: "gautam159@gmail.com",
+        password: "gautam@357"
+    },
+    {
+        name: "riya",
+        mail: "riya456@gmail.com",
+        password: "riya@654"
+    }
+];
+
+console.log(box)
