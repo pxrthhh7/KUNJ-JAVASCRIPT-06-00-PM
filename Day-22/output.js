5b703b1ce08dbaba6c0befb95ecbb28312b7ca20
pxrthhh7 <parthhadiyal4242@gmail.com>
2025-04-13 16:38:37 +0530

    Delete Day-22 directory

--- Day-22/output.js
+++
@@ -1,8 +0,0 @@
-let savedName = localStorage.getItem("userName");
-let savedEmail = localStorage.getItem("userEmail");
-
-let printName = document.getElementById("name")
-let printEmail = document.getElementById("mail")
-
-printName.innerHTML = "<p>Your Name is : </p>"  + savedName
-printEmail.innerHTML = "<p>Your Email is : </p>"  + savedEmail
