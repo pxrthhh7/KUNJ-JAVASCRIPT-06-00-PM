a62ecb6c0a8daf7bcbce6f916b52deb529b0695e
pxrthhh7 <parthhadiyal4242@gmail.com>
2025-04-13 16:16:16 +0530

    Day-20 (Dark/Light switch mode) Added

---
+++ Day-20/Project-2/script.js
@@ -0,0 +1,9 @@
+// let body = document.querySelector(".bodyy")
+let header = document.querySelector(".headder")
+
+
+function Mode()
+{
+    // document.body.style.backgroundColor = "red";
+    header.style.backgroundColor = "red"
+}
---
+++ Day-20/script.js
@@ -0,0 +1,17 @@
+let navbar = document.querySelector(".navbar")
+let navlink = document.querySelectorAll(".nav-link")
+let hero = document.querySelector(".hero")
+
+function Mode()
+{
+    navbar.style.backgroundColor = 'black'
+    navlink.style.color = 'white'
+    navbar.style.boxShadow = "0px 10px 30px -10px rgb(8, 123, 246)";
+
+
+    // HERO SECTION
+
+    // hero.style.backgroundColor = 'black'
+
+    // HERO SECTION
+}
