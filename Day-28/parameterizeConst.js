cf674dc6a3ce306485e9673891d63eb2d435b03a
pxrthhh7 <parthhadiyal4242@gmail.com>
2025-04-27 15:26:55 +0530

    TODO List Project Added into Project's

---
+++ Day-28/parameterizeConst.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(name, age) {
+        // this.name = name;
+        // console.log(name); // Only logs
+    }
+}
+
+const p = new Person("Alice", 30);
+console.log(p); 
